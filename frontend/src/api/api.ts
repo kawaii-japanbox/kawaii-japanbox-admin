e0e7fae032da80dd0f870a2c14e2e59a9b6db435
Sabina Zaripova <sabina.zaripova@nu.edu.kz>
2025-02-10 22:01:44 +0500

    edit: remove extra logs

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -8,14 +8,12 @@ import { Role } from "../components/UserManagement/data";
 const API = axios.create({
   baseURL: `${process.env.REACT_APP_BASE_URL}/api`,
 });
-console.log(process.env.REACT_APP_BASE_URL);
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-  console.log("token:", token);
   return config;
 });
 

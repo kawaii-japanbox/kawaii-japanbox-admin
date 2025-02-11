4db2790428a558c3f177432a6b81006d22b92bf3
Sabina Zaripova <sabina.zaripova@nu.edu.kz>
2025-02-12 01:54:09 +0500

    edit: update hooks

--- frontend/src/hooks/useAuth.tsx
+++ frontend/src/hooks/useAuth.tsx
@@ -1,22 +1,8 @@
-// UserContext.tsx
 import React, { createContext, useContext, useEffect, useState } from "react";
-import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import { login } from "../api/api";
 import { rolePermissions, Role } from "../Sidebar";
-// // Define the user type
-// interface User {
-//   userId: string;
-//   role: string;
-// }
-
-// enum AuthRole {
-//   Admin = "admin",
-//   Sales = "sales",
-//   Packaging = "packaging",
-//   Delivery = "delivery",
-// }
 
 export type Page =
   | "dashboard"
@@ -30,13 +16,6 @@ interface DecodedToken {
   userId: string;
   role: string;
 }
-
-// const roles: Record<AuthRole, Page[]> = {
-//   [AuthRole.Admin]: ["dashboard", "settings", "users"],
-//   [AuthRole.Sales]: ["dashboard", "reports"],
-//   [AuthRole.Packaging]: ["orders"],
-//   [AuthRole.Delivery]: ["orders", "tracking"],
-// };
 
 interface User {
   userId: string;
@@ -55,10 +34,6 @@ interface AuthContextType {
 }
 
 const AuthContext = createContext<AuthContextType | null>(null);
-// interface UserContextType {
-//   user: User | null;
-//   setUser: React.Dispatch<React.SetStateAction<User | null>>;
-// }
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
@@ -85,7 +60,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
       setUser({ userId: decoded.userId, role: decoded.role as Role });
       localStorage.setItem("user", JSON.stringify(response));
       localStorage.setItem("token", response.accessToken);
-      navigate("/home");
+      navigate("/");
     } catch (err) {
       console.error("Login failed:", err);
     }
@@ -116,60 +91,3 @@ export const useAuth = () => {
   if (!context) throw new Error("useAuth must be used within an AuthProvider");
   return context;
 };
-
-// Custom hook to use the UserContext
-// export const useUser = (): UserContextType => {
-//   const context = useContext(userContext);
-//   if (!context) {
-//     throw new Error("useUser must be used within a UserProvider");
-//   }
-//   return context;
-// };
-
-// function useProvideAuth() {
-//   const [user, setUser] = useState(null);
-//   const [loading, setLoading] = useState(true);
-
-// const handleUser = (rawUser) => {
-//   if (rawUser) {
-//     const user = formatUser(rawUser);
-//     setUser(user);
-//     setLoading(false);
-//     return user;
-//   } else {
-//     setUser(null);
-//     setLoading(false);
-//     return null;
-//   }
-// };
-
-// const login = async (
-//   email: string,
-//   password: string,
-//   rememberMe: boolean
-// ) => {
-//   try {
-//     const response = await login(email, password, rememberMe);
-//     localStorage.setItem("user", JSON.stringify(response));
-//     navigate("/dashboard");
-//   } catch (error) {
-
-//   }
-
-// };
-
-// const signout = async () => {
-//   // await firebase.auth().signOut();
-//   handleUser(null);
-// };
-
-//   return { user, loading, signout };
-// }
-
-// const formatUser = (user) => ({
-//   uid: user.uid,
-//   email: user.email,
-//   name: user.displayName,
-//   provider: user.providerData[0].providerId,
-//   photoUrl: user.photoURL,
-// });

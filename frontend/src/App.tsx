1ad53bad3faba9dd46ac32d9b1ac039dc44eb15e
Sabina Zaripova <sabina.zaripova@nu.edu.kz>
2025-02-13 00:56:46 +0500

    edit: update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,46 +2,45 @@ import React, { useEffect, useState } from "react";
 import "./index.css";
 import LoginPage from "./LoginPage";
 import Dashboard from "./Dashboard";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 import Unauthorized from "./Unauthorized";
 import { AuthProvider } from "./hooks/useAuth";
 import UserDashboardPage from "./components/UserManagement/UserDashboardPage";
 import OrderDashboardPage from "./components/OrderManagement/OrderDashboardPage";
-import HomePage from "./HomePage";
+import RoleProtectedRoute from "./RoleProtectedRoute";
 
 const App: React.FC = () => {
   return (
     <Router>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Dashboard />} />
+          </Route>
           <Route path="/login" element={<LoginPage />} />
           <Route
             path="/users"
             element={
-              <ProtectedRoute page="users">
+              <RoleProtectedRoute page="users">
                 <UserDashboardPage />
-              </ProtectedRoute>
+              </RoleProtectedRoute>
             }
           />
           <Route
             path="/orders"
             element={
-              <ProtectedRoute page="orders">
+              <RoleProtectedRoute page="orders">
                 <OrderDashboardPage />
-              </ProtectedRoute>
+              </RoleProtectedRoute>
             }
           ></Route>
           <Route path="/unauthorized" element={<Unauthorized />} />
-          {/* <Route
-            path="/"
-            element={
-              <ProtectedRoute page="/">
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          /> */}
         </Routes>
       </AuthProvider>
     </Router>

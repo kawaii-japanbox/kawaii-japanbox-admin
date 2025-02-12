f2e322c70b73c0861b893ab0e07dde780ff7e961
Sabina Zaripova <sabina.zaripova@nu.edu.kz>
2025-02-13 00:57:36 +0500

    add: add sample dashboard ui

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -1,32 +1,37 @@
 import React, { useEffect } from "react";
-import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import { useAuth } from "./hooks/useAuth";
-import UserDashboard from "./components/UserManagement/UserDashboardPage";
 import Layout from "./Layout";
 
 const Dashboard: React.FC = () => {
   const { user } = useAuth();
-  console.log("user received in dashboard:", user);
   if (!user) {
     return <div>Loading...</div>;
   }
 
   return (
-    <>
-      <Layout>
-        <main className="flex-1 p-6 overflow-auto bg-gray-100">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <h1 className="text-2xl font-bold">Welcome to the Dashboard</h1>
-              }
-            />
-            <Route path="/users" element={<UserDashboard />} />
-          </Routes>
+    <Layout>
+      <div className="p-6 bg-gray-50 w-full">
+        <main className="flex-1 p-6">
+          <h1 className="text-2xl font-semibold">Dashboard Overview</h1>
+
+          {/* KPI Cards */}
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <div className="p-4 bg-white shadow-md rounded-lg">
+              <h3 className="text-gray-600">Total Users</h3>
+              <p className="text-2xl font-bold">1234</p>
+            </div>
+            <div className="p-4 bg-white shadow-md rounded-lg">
+              <h3 className="text-gray-600">Orders</h3>
+              <p className="text-2xl font-bold">567</p>
+            </div>
+            <div className="p-4 bg-white shadow-md rounded-lg">
+              <h3 className="text-gray-600">Revenue</h3>
+              <p className="text-2xl font-bold">12345₸</p>
+            </div>
+          </div>
         </main>
-      </Layout>
-    </>
+      </div>
+    </Layout>
   );
 };
 

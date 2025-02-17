4911c1d51eee94ace76805235d8f655aad5f4e2e
Sabina Zaripova <sabina.zaripova@nu.edu.kz>
2025-02-17 10:10:22 +0500

    add: add customer data

--- frontend/src/components/CustomerManagement/data.ts
+++ frontend/src/components/CustomerManagement/data.ts
@@ -10,3 +10,39 @@ export interface IGetCustomersRequest {
   search: string;
   page: number;
 }
+
+export interface IGetCustomerDetailsResponse {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  addresses: Address[];
+  createdAt: string;
+}
+
+interface Address {
+  region: string;
+  country: string;
+  postcode: string;
+  street: string;
+}
+
+export interface IGetCustomerOrdersResponse {
+  map: any;
+  id: string;
+  status: string;
+  deliveryStatus: string;
+  createdAt: string;
+  items: {
+    id: string;
+    product: {
+      name: string;
+    };
+    quantity: number;
+    price: number;
+    currency: string;
+    deliveryCost: number;
+    status: string;
+    createdAt: string;
+  }[];
+}

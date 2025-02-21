f96ecca5c1b01e0abea1779919d07cdd4b8077b0
Sabina Zaripova <sabina.zaripova@nu.edu.kz>
2025-02-21 21:40:19 +0500

    edit: remove comment

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Optional: Add global styles if needed
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
